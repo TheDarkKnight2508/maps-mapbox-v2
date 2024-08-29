97476cf3d4685ae7951b7263ebe4836bd48e5b9b
TheDarkKnight2508 <153277524+TheDarkKnight2508@users.noreply.github.com>
2024-08-29 05:46:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,23 +9,15 @@ MAPBOX_TOKEN = 'pk.eyJ1Ijoid2lwb2RydmNlIiwiYSI6ImNsdnVzN255YzE5MDYycm55c3hheDhtd
 def index():
     return render_template('index.html', mapbox_token=MAPBOX_TOKEN)
 
-@app.route('/geocode')
-def geocode():
-    address = request.args.get('address')
-    response = requests.get(f'https://api.mapbox.com/geocoding/v5/mapbox.places/{address}.json?access_token={MAPBOX_TOKEN}')
-    data = response.json()
-    coordinates = data['features'][0]['geometry']['coordinates']
-    print(coordinates)
-    return jsonify({'coordinates': coordinates})
-
 @app.route('/directions')
 def directions():
     start = request.args.get('start')
     end = request.args.get('end')
-    response = requests.get(f'https://api.mapbox.com/directions/v5/mapbox/driving/{start};{end}?geometries=geojson&overview=full&steps=true&access_token={MAPBOX_TOKEN}')
-    data = response.json()
-    route = data['routes'][0]['geometry']
-    return jsonify({'route': route})
+    response = requests.get(
+        f'https://api.mapbox.com/directions/v5/mapbox/driving/{start};{end}?geometries=geojson&overview=full&steps=true&access_token={MAPBOX_TOKEN}')
+    
+    data = response.json()  # This is the entire response from the Mapbox API
+    return jsonify(data)  # Return the entire response as JSON
 
 
 
